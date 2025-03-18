66d3e3e2994db474621e56efb911543aeb703e3e
Appuhamy2001 <alokakawshal2001@gmail.com>
2025-03-18 12:27:28 +0530

    customer

--- BACKEND/models/Customer.js
+++ BACKEND/models/Customer.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');//hello
 const bcrypt = require('bcryptjs'); // Import bcrypt for password hashing
 
 //Correct capitalization of Schema
